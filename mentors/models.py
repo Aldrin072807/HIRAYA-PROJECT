3aa7132c702aab4025eee5dc52e8e9ad736c7314
Frio <zggarcia@student.hau.edu.ph>
2026-09-05 20:11:02 +0800

    added mentor profile model (after fixing missing model def haha

--- mentors/models.py
+++ mentors/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MentorProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="mentor_profile"
+    )
+    bio = models.TextField(blank=True)
+    expertise = models.TextField(help_text="Enter skills separated by commas.")
+    experience = models.TextField(blank=True)
+    career_field = models.CharField(max_length=100)
+    availability = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
