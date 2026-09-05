fa0f1147395c6b6c227b1395e6162a7e09c3e51a
Zanjo24 <asmeneses1@student.hau.edu.ph>
2026-09-05 14:47:45 +0800

    create user registration view, form, and template

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib.auth import login
 from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You can now log in.')
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+            return redirect('profiles:register') 
     else:
         form = UserRegisterForm()
     return render(request, 'profiles/register.html', {'form': form})
